f4ada0a9c8466b59e3e52cbdae2109b43e268a51
KlimVladMaks <vkm.blog@yandex.ru>
2025-08-07 13:20:59 +0300

    Commit 2

---
+++ src/main/java/me/klimvlad/moneytransfer/server/MoneyTransferServerApplication.java
@@ -0,0 +1,13 @@
+package me.klimvlad.moneytransfer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTransferServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/klimvlad/moneytransfer/server/MoneyTransferServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.klimvlad.moneytransfer.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
